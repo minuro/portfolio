27d015985ad9e68c2fabce244841d9b72b32a657
minuro <kotoribuki@gmail.com>
2019-12-12 00:01:26 +0900

    [update] Homeの内容作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="home" style="height: 80vh;">
-    <h1>home page</h1>
-  </div>
+  <v-container text-center>
+    <v-layout row wrap justify-center>
+      <v-flex xs12 mt-2>
+        <v-card class="pa-3" height="80vh">
+          <h1>Minuro's Portfolio Site</h1>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
