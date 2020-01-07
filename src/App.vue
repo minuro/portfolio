ee97ee56a4d9d71b5583f9d878e1f2b1b54396b6
minuro <kotoribuki@gmail.com>
2020-01-08 00:49:52 +0900

    [update] 遷移ボタンのレスポンシブ対応 サイドバーの作成

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,37 @@
         <span>minuro's portfolio</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-        <LinkButton class="mr-2"
-        v-for="(btn, index) in buttons" 
-        :key="index"
-        :link="btn.link"
-        :name="btn.name"
-        />
+      <LinkButton class="mr-2 hidden-sm-and-down"
+      v-for="(btn, index) in buttons" 
+      :key="index"
+      :link="btn.link"
+      :name="btn.name"
+      />
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer=!drawer"/>
     </v-app-bar>
+    <v-navigation-drawer
+      v-show="drawer"
+      v-model="drawer"
+      :right="right"
+      absolute
+      temporary
+    >
+      <v-list>
+        <template v-for="(btn, index) in buttons">
+          <v-list-item :key="btn.name" :to="{name: btn.link}">
+            <v-list-item-content>
+              <v-list-item-title>
+                {{btn.name}}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider
+            v-if="index + 1 < buttons.length"
+            :key="index"
+          ></v-divider>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
       <router-view/>
     </v-content>
@@ -25,6 +49,8 @@ export default {
   name: 'App',
   data () {
     return {
+      drawer: false,
+      right: true,
       buttons: [
         {
           link: 'home',
