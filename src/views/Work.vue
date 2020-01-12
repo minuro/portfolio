c7dd8e60ccdab5b256249cde6aaee1602b2da7d4
minuro <kotoribuki@gmail.com>
2020-01-13 02:41:01 +0900

    [update] work追加

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -5,10 +5,31 @@
         <h1>Work</h1>
       </v-flex>
       <v-flex xs10 mt-2>
-        <v-card class="pa-3">
-          <h3>--- 開発中 ---</h3>
+        <v-card>
+          <v-card v-if="works.length == 0" class="pa-3">
+            <h3>--- 開発中 ---</h3>
+          </v-card>
+          <v-row>
+            <WorkComponent v-for="work in works" :key="work.name" :work="work"/>
+          </v-row>
         </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+import works from '../assets/works.json'
+import WorkComponent from '../components/WorkComponent'
+
+export default {
+  components: {
+    WorkComponent
+  },
+  data() {
+    return {
+      works: works
+    }
+  }
+}
+</script>
